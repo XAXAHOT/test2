625c80f2cdd8e75a3749d75b13e05660aee104da
XAXAHOT <xaxahot2@gmail.com>
2022-02-08 20:02:26 +0300

    Commit message

---
+++ app/src/main/java/com/example/scrollview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.viewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ScrollView scrollView = new ScrollView(this);
+
+        TextView textView1 = new TextView(this);
+        textView1.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry...like Aldus PageMaker including versions of Lorem Ipsum.");
+        textView1.setLayoutParams(new ViewGroup.LayoutParams
+                (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView1.setTextSize(26);
+        scrollView.addView(textView1);
+        setContentView(scrollView);
+    }
+}
